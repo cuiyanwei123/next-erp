ea3fbce172a80d0ae3ccac71b468d84ca0fc4ab7
崔艳伟 <120182154@qq.com>
2019-09-07 13:12:13 +0800

    ant-design-pro-vue version 20190907

--- next-erp-client/src/App.vue
+++ next-erp-client/src/App.vue
@@ -1,11 +1,29 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <a-locale-provider :locale="locale">
+    <div id="app">
+      <router-view/>
+    </div>
+  </a-locale-provider>
 </template>
 
+<script>
+import zhCN from 'ant-design-vue/lib/locale-provider/zh_CN'
+import { AppDeviceEnquire } from '@/utils/mixin'
+
+export default {
+  mixins: [AppDeviceEnquire],
+  data () {
+    return {
+      locale: zhCN
+    }
+  },
+  mounted () {
+
+  }
+}
+</script>
 <style>
-#app {
+  #app {
     height: 100%;
   }
 </style>
